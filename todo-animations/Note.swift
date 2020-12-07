d72a87001384d5b5d70b4e12f64eddd7cf32f48a
Mohamed Dabbour <ortiiga18@Samis-MacBook-Air.local>
2020-12-08 00:19:06 +0200

    Created UI

---
+++ todo-animations/Note.swift
@@ -0,0 +1,27 @@
+//
+//  Note.swift
+//  todo-animations
+//
+//  Created by Mohamed Dabbour on 12/8/20.
+//
+
+import Foundation
+
+struct Note : Codable{
+    var title:String
+    var completed:Bool
+    var createdAt:Date
+    var Id:UUID
+    
+    func saveItem() {
+        
+    }
+    
+    func deleteItem() {
+        
+    }
+    
+    func markAsCompleted() {
+        
+    }
+}
